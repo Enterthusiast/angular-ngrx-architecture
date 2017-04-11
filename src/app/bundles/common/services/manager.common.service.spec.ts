9d5a1feffcf93434389e84433d31307820fb344d
dbernard <dbernard@leadersleague.com>
2017-04-11 12:27:07 +0200

    test cover manager, but we don't test much in manager because it's mostly an aggreagation of every service already texted, there is nearly no logic there

---
+++ src/app/bundles/common/services/manager.common.service.spec.ts
@@ -0,0 +1,95 @@
+import {TestBed, async, inject, fakeAsync} from '@angular/core/testing';
+import {Injectable} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {StoreModule} from '@ngrx/store';
+import {Http, HttpModule, Response, ResponseOptions, XHRBackend} from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
+
+import 'rxjs/add/operator/last';
+import * as _ from 'lodash';
+
+import {SubjectCommonService} from './subject.common.service';
+import {ManagerCommonService} from './manager.common.service';
+import {ApiCommonService} from './api.common.service';
+import {TransformerCommonService} from './transformer.common.service';
+import {ItemCommonService} from './item.common.service';
+import {ItemFactoryCommonService} from './item.factory.common.service';
+import {EffectCommonService} from './effect.common.service';
+import {ItemCommonClass} from '../models/item.common.class';
+import {ItemCommonConfig} from '../../../configs/models/item.common.config';
+
+
+// create dummy services
+const testManagerParams = {
+  embeddedListKey: 'test-list'
+};
+
+@Injectable()
+export class TestManagerCommonService extends ManagerCommonService {
+
+  constructor(protected apiService: ApiCommonService,
+              protected transformerService: TransformerCommonService,
+              protected factoryService: ItemFactoryCommonService,
+              protected effectService: EffectCommonService) {
+    super(apiService,
+          transformerService,
+          factoryService,
+          effectService);
+  }
+
+}
+
+// start testing
+describe('ManagerCommonService', () => {
+
+  const storeList = [
+    new ItemCommonClass({ [ItemCommonConfig.ATTRIBUTES] : { uuid: 1, name: 'John'} }),
+    new ItemCommonClass({ [ItemCommonConfig.ATTRIBUTES] : { uuid: 2, name: 'Jane'} }),
+    new ItemCommonClass({ [ItemCommonConfig.ATTRIBUTES] : { uuid: 3, name: 'Taekwon'} })
+  ];
+
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      imports: [
+        HttpModule,
+        StoreModule.provideStore({
+          test: (state = { list: storeList, watchedId: 2 }, action ) => {
+            switch (action.type) {
+              case 'SET_LIST':
+                return _.assignIn({}, state, { list: action.payload });
+              case 'SET_WATCHEDID':
+                return _.assignIn({}, state, { watchedId: action.payload });
+              default:
+                return state;
+            }
+          }
+        })
+      ],
+      providers: [
+        ApiCommonService,
+        EffectCommonService,
+        ManagerCommonService,
+        ItemCommonService,
+        ItemFactoryCommonService,
+        ManagerCommonService,
+        SubjectCommonService,
+        TransformerCommonService,
+        TestManagerCommonService,
+        { provide: XHRBackend, useClass: MockBackend }
+      ]
+    });
+
+  });
+
+  it('should be able to initialise the service',
+    inject([ManagerCommonService], (subjectCommonService) => {
+      expect(subjectCommonService).toBeTruthy();
+    }));
+
+  it('should be able to initialize a children service class',
+    inject([TestManagerCommonService], (testManagerCommonService) => {
+      expect(testManagerCommonService).toBeTruthy();
+    }));
+
+});
